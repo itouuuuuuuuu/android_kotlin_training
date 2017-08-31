7446a16c5ece61f08bbcbe8630aedc6bc42e792a
itouuuuuuuuu <mfito.13@gmail.com>
2017-08-31 19:04:05 +0900

    TabLayout + ViewPager実装サンプル

---
+++ TabLayoutViewPagerSampleApp/README.md
@@ -0,0 +1,6 @@
+* PageView
+* TabLayout
+* fragment
+
+参考
+http://qiita.com/Yuki_Yamada/items/6d8b38effeb38ed96d7i8
---
+++ TabLayoutViewPagerSampleApp/app/src/main/java/com/example/masafumi_ito/pageviewsample/MyFragmentStatePagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.masafumi_ito.pageviewsample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Masafumi_Ito on 2017/08/31.
+ */
+
+public class MyFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    public MyFragmentStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+
+        switch (i) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            default:
+                return new ThirdFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page " + position;
+    }
+}
